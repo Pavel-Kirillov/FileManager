12a3f5ccc480e69e0d9c52c7fcf2c410ef3c0a96
Pavel Kirillov <pavel.m.kirillov@gmail.com>
2021-03-26 21:02:01 +0300

    Урок 1

---
+++ lesson-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Как вас зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToString("dd MMMM yyyy") }");
+
+        }
+    }
+}
